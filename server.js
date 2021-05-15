ba4274c2ec3151de504c49cf6d0bf05d0c3e9f6f
contractorakshar <contractorakshar8319@gmail.com>
2021-05-15 18:19:22 +0530

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = "/index.html";
 
 const server = express()
-  .use((req, res) => res.redirect(INDEX, { root: __dirname }))
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
